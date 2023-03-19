bec4cbd6871ec890659007d759a00ea41a342bf6
Harshit2009 <09harshitgoel@gmail.com>
2023-03-19 12:31:05 +0530

    Second Commit

--- pages/views.py
+++ pages/views.py
@@ -10,7 +10,13 @@ def contact_view(request, *args, **kwargs):
     return render(request, "contact.html", {})
 
 def about_view(request,*args, **kwargs):
-    return render(request, "about.html", {})
+    my_context = {
+        "my_text": "This is about us",
+        "this_is_true": True,
+        "my_number": 123,
+        "my_list": [123, 456, 789]
+    }
+    return render(request, "about.html", my_context)
 
 def social_view(request, *args, **kwargs):
     return HttpResponse ("<h1>Social Page</h1>")
